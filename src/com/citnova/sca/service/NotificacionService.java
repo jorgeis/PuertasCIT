68964fcf0f5231a710c504c9b10084d5acdb91d2
abrahamramirez <abraham.ramirez.salgado@hotmail.com>
2017-03-22 11:40:02 -0600

    Método queryAllByStatusNot añadido
    * Capas de servicio y repositorios añadidas

--- src/com/citnova/sca/service/NotificacionService.java
+++ src/com/citnova/sca/service/NotificacionService.java
@@ -1,9 +1,14 @@
 package com.citnova.sca.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.citnova.sca.domain.Notificacion;
 import com.citnova.sca.repository.NotificacionRepository;
+import com.citnova.sca.util.Constants;
 
 @Service
 public class NotificacionService {
@@ -12,5 +17,7 @@ public class NotificacionService {
 	private NotificacionRepository notificacionRepository;
 	
 	
-	
+	public Page<Notificacion> getPage(int index) {
+		return notificacionRepository.findByStatus(Constants.STATUS_ACTIVE, new PageRequest(index, Constants.ITEMS_PER_PAGE, Direction.ASC, "idNot"));
+	}
 }
