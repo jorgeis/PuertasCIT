d317fe114a482bbc7dfada2319c3d3568afc090f
abrahamramirez <abraham.ramirez.salgado@hotmail.com>
2017-03-22 11:34:33 -0600

    NotificacionRepo Añadido

---
+++ src/com/citnova/sca/repository/NotificacionRepository.java
@@ -0,0 +1,9 @@
+package com.citnova.sca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.citnova.sca.domain.Notificacion;
+
+public interface NotificacionRepository extends JpaRepository<Notificacion, Integer>{
+
+}
