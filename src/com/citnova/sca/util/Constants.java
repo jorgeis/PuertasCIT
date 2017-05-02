0b9eabe25bc0ed0ea3747a4265af0433fb587730
jorgeis <jorgemau4@yahoo.com.mx>
2017-05-02 17:19:17 -0500

    Prueba Commit
    Agregada constante en Constants.java

--- src/com/citnova/sca/util/Constants.java
+++ src/com/citnova/sca/util/Constants.java
@@ -56,6 +56,9 @@ public class Constants {
 	public static final String EMPRESARIAL = "Empresarial";
 	
 	public static final String PENDING_PAYMENT = "Pendiente de pago";
+	public static final String STRING_TEST = "Cadena de prueba";
+	
+	
 	
 	
 }
